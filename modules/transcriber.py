7bad8c33dfdfb45ff6c7d22257b7657340938337
Pulugala Manikanta <manikantapulugala@gmail.com>
2026-05-30 17:58:14 +0530

    transcriber.py

--- modules/transcriber.py
+++ modules/transcriber.py
@@ -1,45 +1,19 @@
-from modules.gemini_helper import model
-import google.generativeai as genai
-import time
+from faster_whisper import WhisperModel
 
 
-def transcribe_audio(audio_path):
-    retries = 3
+model = WhisperModel(
+    "base",
+    device="cpu",
+    compute_type="int8"
+)
 
-    for attempt in range(retries):
-        try:
-            uploaded_file = genai.upload_file(audio_path)
 
-            prompt = """
-            Transcribe this meeting audio accurately.
+def transcribe_audio(file_path):
+    segments, _ = model.transcribe(file_path)
 
-            Rules:
-            1. Return only transcript.
-            2. Keep speaker labels if identifiable.
-            3. Remove timestamps.
-            4. Do not summarize.
-            5. Preserve conversation flow.
-            6. Do not invent speaker names.
+    transcript = "\n".join(
+        segment.text
+        for segment in segments
+    )
 
-            Example:
-
-            Manager:
-            Good morning everyone.
-
-            Developer:
-            I'll optimize SQL queries.
-            """
-
-            response = model.generate_content(
-                [prompt, uploaded_file]
-            )
-
-            return response.text
-
-        except Exception as e:
-            print(f"Attempt {attempt+1} failed: {e}")
-
-            if attempt < retries - 1:
-                time.sleep(5)
-            else:
-                raise e
+    return transcript
