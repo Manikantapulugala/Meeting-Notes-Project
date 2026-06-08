fd35e757010eb87ad207d9291eccfda43d6348a3
Pulugala Manikanta <manikantapulugala@gmail.com>
2026-06-08 15:15:26 +0530

    gemini_helper.py

--- modules/gemini_helper.py
+++ modules/gemini_helper.py
@@ -1,13 +1,17 @@
-import google.generativeai as genai
 from dotenv import load_dotenv
 import os
 
+from langchain_google_genai import ChatGoogleGenerativeAI
+
 load_dotenv()
 
-genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
-model = genai.GenerativeModel("gemini-2.5-flash-lite")
+model = ChatGoogleGenerativeAI(
+    model="gemini-2.5-flash",
+    google_api_key=os.getenv("GEMINI_API_KEY"),
+    temperature=0.3
+)
 
 
 def ask_gemini(prompt):
-    response = model.generate_content(prompt)
-    return response.text
+    response = model.invoke(prompt)
+    return response.content
